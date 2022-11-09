6331427963b392adfe74c19268b0f957783b3412
abhishekBhangalia2471 <116834189+abhishekBhangalia2471@users.noreply.github.com>
2022-11-09 23:23:57 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -9,58 +9,66 @@ typedef struct TNode{
 }tNode;
 
 void BST_Insert(treeptr root, int val);
-//void BST_Delete(treeptr root, int val);
+void BST_Delete(treeptr root, int val);
 void Inorder_Trav(treeptr root);
-//void Preorder_Trav(treeptr root);
-//void Postorder_Trav(treeptr root);
+void Preorder_Trav(treeptr root);
+void Postorder_Trav(treeptr root);
 
 int main(int argc, char *argv[])
 {
+    printf("\n...................Binary Search Tree..................");
 	treeptr root = (treeptr) malloc(sizeof(tNode));
 	root->parent = NULL;
 	root->key=0;
 	root->parent = root->lchild=root->rchild=NULL;
-	int choice,val;
-	printf("\nEnter choice");
-		scanf("%d",&choice);
+	int choice,val;  
 	
 	while(choice!=0){
-		printf("\nEnter choice");
+		printf("\n\nSelect your operation :\n\n1. Insert element\n2. Delete element\n3. Inorder Traversal\n4. Preorder Traversal\n5. Postorder Traversal\n0. QUIT\n\nEnter your choice here (0-5) : ");
 		scanf("%d",&choice);
 		if(choice==1){
-			printf("\nEnter element ");
-			
+			printf("\nEnter the number that you want to insert into BST : ");
 			scanf("%d",&val);
 			BST_Insert(root,val);
-			
 		}
 		else if(choice == 2){
-			printf("Enter element to delete");
+			printf("\nEnter the number that you want to delete from BST : ");
 			scanf("%d",&val);
-		//	BST_Delete(root,val);
+			BST_Delete(root,val);
 		}
 		else if(choice ==3){
-			Inorder_Trav(root);
+            printf("\nInorder Traversal : ");
+            if (root->rchild != NULL)
+			    Inorder_Trav(root->rchild);
+            else
+                printf("BST is empty !!!");
 		}
 		else if(choice == 4){
-		//	Preorder_Trav(root->rchild);
+            printf("\nPreorder Traversal : ");
+            if(root->rchild != NULL)
+			    Preorder_Trav(root->rchild);
+            else 
+                printf("BST is empty !!!");
 		}
 		else if(choice == 5){
-		//	Postorder_Trav(root->rchild);
+            printf("\nPostorder Traversal : ");
+            if(root->rchild != NULL)
+			    Postorder_Trav(root->rchild);
+            else 
+                printf("BST is empty !!!");
 		}
 		else if (choice ==0){
-			
+			printf("\n\nQuiting...");
 			return 0;
 		}
 	}
-	
 }
 
-void BST_Insert(treeptr root, int x){
+void BST_Insert(treeptr root, int val){
 	treeptr tmp,cur,prev;
 	tmp = (treeptr)malloc(sizeof(tNode));
 	if(tmp!=NULL){
-	tmp->key =x;
+	tmp->key =val;
 	tmp->lchild=NULL;
 	tmp->rchild=NULL;
 	root->key--;
@@ -69,14 +77,14 @@ void BST_Insert(treeptr root, int x){
 	cur=root;
 	while(cur!=NULL){
 		prev=cur;
-		if(tmp->key<cur->key){
+		if(val<cur->key){
 			cur=cur->lchild;
 		}
 		else{
 			cur =cur->rchild;
 		}
 	}
-	if(tmp->key<prev->key){
+	if(val<prev->key){
 		prev->lchild = tmp;
 		tmp->parent = prev;
 	}
@@ -86,11 +94,89 @@ void BST_Insert(treeptr root, int x){
 	}
 }
 
+
+void BST_Delete(treeptr root, int val){
+    treeptr tmp, cur, prev;
+    prev = NULL;
+    cur = root;
+    while(cur != NULL && cur->key != val){
+        prev = cur;
+        if(val < cur->key){
+            cur = cur->lchild;
+        }
+        else {
+            cur = cur->rchild;
+        }
+    }
+
+    if(cur != NULL){
+        root->key++;
+        if(cur->lchild != NULL && cur->rchild != NULL){
+            prev = cur;
+            tmp = cur;
+            cur = cur->rchild;
+            while (cur->lchild != NULL){
+                prev = cur;
+                cur = cur->lchild;
+            }
+            tmp->key = cur->key;
+        }
+        tmp = cur;
+        if(cur == prev->lchild){
+            if(cur->rchild!=NULL){
+                prev->lchild = cur->rchild;
+                cur->rchild->parent = prev;
+            }
+            else if(cur->lchild != NULL){
+                prev->lchild = cur->lchild;
+                cur->lchild->parent = prev;
+            }
+            else{
+                prev->lchild = NULL;
+            }
+
+        }
+        else if(cur == prev->rchild){
+            if(cur->rchild!=NULL){
+                prev->rchild = cur->rchild;
+                cur->rchild->parent = prev;
+            }
+            else if(cur->lchild != NULL){
+                prev->rchild = cur->lchild;
+                cur->lchild->parent = prev;
+            }
+            else{
+                prev->rchild = NULL;
+            }
+        }
+        free(tmp);
+    
+    }
+    else {
+        printf("\nNumber not found in BST !!!");
+    }
+}
+
 void Inorder_Trav(treeptr root){
-	while(root!=NULL){
+	if(root!=NULL){
 		Inorder_Trav(root->lchild);
-		printf("%d",root->key);
+		printf("%d ",root->key);
 		Inorder_Trav(root->rchild);
 	}
 }
-	
+
+void Preorder_Trav(treeptr root){
+	if(root!=NULL){
+		printf("%d ",root->key);
+        Preorder_Trav(root->lchild);
+		Preorder_Trav(root->rchild);
+	}
+}
+
+void Postorder_Trav(treeptr root){
+	if(root!=NULL){
+		Postorder_Trav(root->lchild);
+		Postorder_Trav(root->rchild);
+        printf("%d ",root->key);
+	}
+}
