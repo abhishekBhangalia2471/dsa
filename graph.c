05251e79928baea6aa30bcf08f1d31eca0c79da6
abhishekBhangalia2471 <116834189+abhishekBhangalia2471@users.noreply.github.com>
2022-12-08 12:42:10 +0530

    Add files via upload

--- graph.c
+++ graph.c
@@ -1,57 +1,154 @@
-struct queue* createQueue() {
-  struct queue* q = malloc(sizeof(struct queue));
-  q->front = -1;
-  q->rear = -1;
-  return q;
-}
+#include <stdio.h>
+#include <stdlib.h>
 
-// Check if the queue is empty
-int isEmpty(struct queue* q) {
-  if (q->rear == -1)
-    return 1;
-  else
+typedef struct vertex* vertex_ptr;
+typedef struct vertex{
+    int val;
+    int distance;
+    vertex_ptr next;
+}vertex_node; 
+
+typedef struct q_node* q_ptr;
+typedef struct q_node{
+    int val;
+    q_ptr next;
+}qNode;
+
+q_ptr front=NULL,rear=NULL;
+
+void insertQ(int);
+int deleteQ();
+void displayQ();
+
+
+void insertEdge(int,int);
+void displayGraph();
+
+
+
+vertex_ptr* graph;
+int size;
+
+int main(){
+    
+    printf("\nEnter size of graph : ");
+    scanf("%d",&size);
+    graph = (vertex_ptr*) malloc(sizeof(vertex_node) * size);
+
+    insertEdge(0,1);
+    insertEdge(0,5);
+    insertEdge(1,2);
+    insertEdge(1,5);
+    insertEdge(2,3);
+    insertEdge(2,4);
+    insertEdge(2,5);
+    insertEdge(3,4);
+    insertEdge(4,5);
+    displayGraph();
+    BFS(0);
+    
+    printf("\n");
     return 0;
 }
 
-// Adding elements into queue
-void enqueue(struct queue* q, int value) {
-  if (q->rear == SIZE - 1)
-    printf("\nQueue is Full!!");
-  else {
-    if (q->front == -1)
-      q->front = 0;
-    q->rear++;
-    q->items[q->rear] = value;
-  }
+void insertEdge(int a, int b){
+    vertex_ptr tmp = (vertex_ptr) malloc(sizeof(vertex_node));
+    tmp->val = b;
+    tmp->next = graph[a];
+    graph[a] = tmp;
+    
+    vertex_ptr tmp2 = (vertex_ptr) malloc(sizeof(vertex_node));
+    tmp2->val = a;
+    tmp2->next = graph[b];
+    graph[b] = tmp2;
+    
 }
 
-// Removing elements from queue
-int dequeue(struct queue* q) {
-  int item;
-  if (isEmpty(q)) {
-    printf("Queue is empty");
-    item = -1;
-  } else {
-    item = q->items[q->front];
-    q->front++;
-    if (q->front > q->rear) {
-      printf("Resetting queue ");
-      q->front = q->rear = -1;
+
+
+void displayGraph(){
+    for(int i = 0; i<size; i++){
+        printf("\nAdjacent vertices of %d : ",i);
+        vertex_ptr tmp = graph[i];
+        while(tmp){
+            printf("%d ",tmp->val);
+            tmp = tmp->next;
+        }
+        printf("\n");
     }
-  }
-  return item;
 }
 
-// Print the queue
-void printQueue(struct queue* q) {
-  int i = q->front;
-
-  if (isEmpty(q)) {
-    printf("Queue is empty");
-  } else {
-    printf("\nQueue contains \n");
-    for (i = q->front; i < q->rear + 1; i++) {
-      printf("%d ", q->items[i]);
+void insertQ(int x){
+    q_ptr tmp = (q_ptr) malloc(sizeof(q_ptr));
+    tmp->val = x;
+    tmp->next = NULL;
+    if(rear == NULL){
+        front = rear = tmp;
     }
-  }
+    else{
+        rear->next = tmp;
+        rear = tmp;
+    }
 }
+
+int deleteQ(){
+    if(front == NULL){
+        return -1;
+    }
+    q_ptr tmp = front;
+    front = front->next;
+    if (front == NULL)
+        rear = NULL;
+    int x = tmp->val;
+    free(tmp);
+    return x;
+}
+
+
+void displayQ(){
+    q_ptr tmp = front;
+    while(tmp){
+        printf("%d",tmp->val);
+        tmp=tmp->next;
+    }
+}
+
+void BFS(int s){
+    printf("\n\nBFS(%d) :\n\n",s);
+    int p[size],d[size];
+    char c[size];
+    
+    for(int i=0; i<size; i++){
+        p[i] = NULL;
+        c[i] = 'w';
+        d[i] = size;
+    }
+    d[s] = 0;
+    c[s] = 'g';
+    insertQ(s);
+    displayQ();
+    while(rear!=NULL){
+        int u = deleteQ();
+        vertex_ptr tmp = graph[u];
+        while(tmp){
+            int v = tmp->val;
+            if(c[v] == 'w'){
+                printf("%d ",v);
+                p[v] = u;
+                d[v] = d[u] + 1;
+                c[v] = 'g';
+                insertQ(v);
+            }
+            tmp = tmp->next;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
