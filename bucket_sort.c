95ac3615e9ea7f347415b69efbd1b492b4fcb3cb
abhishekBhangalia2471 <116834189+abhishekBhangalia2471@users.noreply.github.com>
2022-12-18 19:29:04 +0530

    Add files via upload

--- bucket_sort.c
+++ bucket_sort.c
@@ -1,34 +1,62 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
 typedef struct Node{
     float val;
-    node* next;
+    Node* next;
 }node;
 
+
 int main(){
-    int min, max;
-    max = INT_MIN;
-    min = INT_MAX;
+    float min, max;
+    max = FLT_MIN;
+    min = FLT_MAX;
     
     int size;
     printf("enter size : ");
     scanf("%d",&size);
+    node** bucket = (node**)malloc(sizeof(node*) * 10);
+    
     float A[size];
-    for(int i=0; i<size; i++){
-        cin>>A[i];
-        if(A[i] > max)
-            max = A[i];
-        else if (A[i] < min){
-            min = A[i];
-        }
+    for(int i=0;i<size;i++){
+   	bucket[i] = NULL;
     }
-    int range = max-min;
     
     for(int i=0; i<size; i++){
-        int b = (A[i] - min)/range;
-        k = b*size;
+        scanf("%f",&A[i]);
+        	if(A[i] > max)
+            max = A[i];
+        if (A[i] < min){
+            min = A[i];
+        }
         
+    }
+    //printf("min %f, max %f",min,max);
+    
+    float range = max-min;
+    
+    for(int i=0; i<size; i++){
+       // printf("min = %f, range = %f",min,range);
+        float b = (A[i] - min)/range;
+        int k = b*size;
+        //printf("%d",k);
+        if(k == size)
+        	k--;
+        node* temp = (node*) malloc(sizeof(node));
+        temp->val = A[i];
+        temp->next  = bucket[k];
+        bucket[k] = temp;
+    }
+    
+    for(int i=0;i<size;i++){
+    	node* tmp = bucket[i];
+    	printf("\nBucket %d : ",i);
+    	while(tmp){
+    		printf("%f ",tmp->val);
+    		tmp=tmp->next;
+    	};
+    	printf("\n");
     }
     
     
